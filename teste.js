85877dc62b604fdbf80356e6709c30c8d6bb1036
Rafaela-Caviquioni <rafaccaviquioni@gmail.com>
2024-12-28 11:27:56 -0300

    Create teste.js
    Projeto de certificação 1 - Pontuação dos atletas

---
+++ teste.js
@@ -0,0 +1,35 @@
+let atletas = [
+  {
+    nome: 'Cesar Abascal',
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: 'Fernando Puntel',
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: 'Daiane Jelinsky',
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: 'Bruno Castro',
+    notas: [10, 10, 10, 9, 9.5]
+  }
+]
+for (let i = 0; i < atletas.length; i++) {
+  let atleta = atletas[i]
+  let notasOrdenadas = atleta.notas.sort((a, b) => a - b)
+
+  let notasMeio = notasOrdenadas.slice(1, 4)
+
+  let notasSomadas = 0
+  notasMeio.forEach(nota => {
+    notasSomadas += nota
+  })
+
+  let mediaFinal = notasSomadas / notasMeio.length
+
+  console.log('Atleta:', atleta.nome)
+  console.log('Notas Obtidas:', atleta.notas)
+  console.log('Média Válida:', mediaFinal)
+}
